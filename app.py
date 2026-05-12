90afb647ca89fabe0334718e6ececfbb17147c0a
34-THÀNH-0047 <148701669+34-THANH-0047@users.noreply.github.com>
2026-05-12 16:18:37 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Cloud PaaS Demo")
+
+st.write("Ứng dụng này được xây dựng bằng Python và Streamlit.")
+
+name = st.text_input("Đinh Công Thành")
+
+if name:
+    st.write(f"Xin chào {name}!")
+
+st.subheader("Ý nghĩa cloud")
+
+st.write("Khi triển khai lên Streamlit Cloud, ứng dụng này sẽ chạy trên nền tảng PaaS.")
